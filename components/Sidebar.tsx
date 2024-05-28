befe79b389256e7b425d486f9fa2488cad1d132d
rimzss <bobbebe40@gmail.com>
2024-05-28 12:24:36 +0800

    left sidebar done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = ({ user }: SiderbarProps) => {
-  return <section className="sidebar"></section>;
+  const pathname = usePathname();
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">N Bank</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                  src={item.imgURL}
+                  fill
+                  alt={item.label}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
 };
 
 export default Sidebar;
