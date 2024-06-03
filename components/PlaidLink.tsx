530b3f2706cd3a4278729f2abe6f2fda79c12505
rimzss <bobbebe40@gmail.com>
2024-06-03 21:57:42 +0800

    transaction history page done

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -10,6 +10,7 @@ import {
   createLinkToken,
   exchangePublicToken,
 } from "@/lib/actions/user.action";
+import Image from "next/image";
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter();
   const [token, setToken] = useState("");
@@ -49,9 +50,40 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
           Connect bank
         </Button>
       ) : variant === "ghost" ? (
-        <Button>Connect bank</Button>
+        <Button
+          onClick={() => {
+            open();
+          }}
+          variant="ghost"
+          className="plaidlink-ghost"
+        >
+          <Image
+            src="/icons/connect-bank.svg"
+            alt="connect bank"
+            width={24}
+            height={24}
+          />
+          <p className="text-[16px] font-semibold text-black-2 hidden xl:block">
+            Connect bank
+          </p>
+        </Button>
       ) : (
-        <Button>Connect bank</Button>
+        <Button
+          onClick={() => {
+            open();
+          }}
+          className="plaidlink-default"
+        >
+          <Image
+            src="/icons/connect-bank.svg"
+            alt="connect bank"
+            width={24}
+            height={24}
+          />
+          <p className="text-[16px] font-semibold text-black-2 ">
+            Connect bank
+          </p>
+        </Button>
       )}
     </>
   );
